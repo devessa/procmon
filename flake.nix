904f92d196c5c0c82ddead9ae9a76d2a0a9e1d1b
dess <hi@dessa.dev>
2024-05-19 13:35:25 -0400

    chore: initialize

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "repo nix flake";
+  outputs = {self, ...} @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        pkgs,
+        lib,
+        self',
+        inputs',
+        ...
+      }: {
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [alejandra nodejs treefmt pre-commit rustc clippy rustfmt cargo rust-analyzer pkg-config];
+          };
+        packages.example = with pkgs;
+          rustPlatform.buildRustPackage {
+            pname = "procmon";
+            inherit ((lib.importTOML ./Cargo.toml).package) version;
+
+            src = lib.sourceByRegex self [
+              "(src)(/.*)?"
+              "Cargo\.(toml|lock)"
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            meta = with lib; {
+              description = "monitor for any changes in a filesystem and automatically restart - perfect for development";
+              license = licenses.mit;
+              platforms = platforms.linux;
+            };
+
+            nativeBuildInputs = [pkg-config];
+            buildInputs = [];
+          };
+        packages.default = self'.packages.example;
+      };
+    };
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+}
